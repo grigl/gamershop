26aeafb062d4bf6979ced2e362eb9b0c2cec32cf
Gleb Grishin <griglmail@gmail.com>
2012-03-02 00:26:42 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gamershop::Application.config.secret_token = 'd7378bc0f0efbf454035d4009557ac4dc2c7452aa95ed6c76a9cf943b0b002452731bdd739403780b395dba1f623f06392f735be571aff81cd092c156054ba17'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Gamershop::Application.config.session_store :cookie_store, key: '_gamershop_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Gamershop::Application.config.session_store :active_record_store
